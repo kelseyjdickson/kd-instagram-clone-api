7e76b2dd49839c2d5e0fe8fe3c501d03cbbd5685
kelseyjdickson <kelseymedved@gmail.com>
2021-10-18 20:03:09 -0400

    edited controller in posts file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,25 @@
 class PostsController < ApplicationController
+    def index
+        render json: Post.all
+    end
+    
     def create
-        binding.pry
-        post = Post.new(post_params)
+        post = Post.create(caption: post_params["caption"])
+        post.avatar.attach(post_params["file"])
+        post.photo_url = url_for(post.avatar)
+        if post.save
+            render json: post
+        else 
+            render json: {
+                error: "Not Workin"
+            }
     end
 end
+
+    private
+    def post_params
+        params.permit(:file,:caption)
+    end
+end
+
+
